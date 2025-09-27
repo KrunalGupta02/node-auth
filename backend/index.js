185acebdf7fe1364beb8f430c76ac2716a967246
Krunal Gupta <guptakrunal02@gmail.com>
2025-09-27 14:02:56 +0530

    Setup the backend files and structure

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./db/connectDB.js";
+import authRoutes from "./routes/auth.route.js";
 
+dotenv.config();
 const app = express();
 
+app.use('/api/v1/auth', authRoutes)
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(process.env.PORT || 5000, () => {
+    connectDB();
+    console.log(`Server is running on port ${process.env.PORT || 5000}`);
 })
