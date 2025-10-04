5800b9de83ccd4f1681d70b7891c1dd8ca40f9e9
Krunal Gupta <guptakrunal02@gmail.com>
2025-10-04 13:13:51 +0530

    Initiate the zustand and integrate the signup functionality

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,17 +1,29 @@
 import { motion } from "framer-motion";
 import Input from "../components/Input";
-import { Lock, Mail, User } from "lucide-react";
+import { Loader, Lock, Mail, User } from "lucide-react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import PasswordStrengthMeter from "../components/PasswordStrengthMeter";
+import { useAuthStore } from "../store/authStore";
 
 const SignupPage = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSignUp = (e) => {
+  const navigate = useNavigate();
+
+  const { signup, error, isLoading } = useAuthStore();
+
+  const handleSignUp = async (e) => {
     e.preventDefault();
+
+    try {
+      await signup(email, password, name);
+      navigate("/verify-email");
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
@@ -51,6 +63,9 @@ const SignupPage = () => {
             onChange={(e) => setPassword(e.target.value)}
           />
 
+          {/* Error */}
+          {error && <p className="text-red-500 font-semibold mt-2">{error}</p>}
+
           {/* Password Strength Meter */}
           <PasswordStrengthMeter password={password} />
 
@@ -59,8 +74,13 @@ const SignupPage = () => {
             whileTap={{ scale: 0.98 }}
             type="submit"
             className="mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-500 text-white font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200 cursor-pointer"
+            disabled={isLoading}
           >
-            Sign Up
+            {isLoading ? (
+              <Loader className="animate-spin mx-auto size-7" />
+            ) : (
+              "Sign Up"
+            )}
           </motion.button>
         </form>
       </div>
---
+++ frontend/src/store/authStore.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { create } from "zustand";
+
+const API_URL = "http://localhost:5000/api/v1/auth";
+
+axios.defaults.withCredentials = true; // this will add cookies to requests
+
+export const useAuthStore = create((set) => ({
+  user: null, // initial state
+  isAuthenticated: false,
+  error: null,
+  isLoading: false,
+  isCheckingAuth: true,
+
+  signup: async (email, password, name) => {
+    set({ isLoading: true, error: null });
+
+    try {
+      const response = await axios.post(`${API_URL}/signup`, {
+        email,
+        password,
+        name,
+      });
+      set({
+        user: response.data.user,
+        isAuthenticated: true,
+        isLoading: false,
+      });
+    } catch (error) {
+      set({
+        error: error.response.data.message || "Error signing up",
+        isLoading: false,
+      });
+      throw error;
+    }
+  },
+}));
